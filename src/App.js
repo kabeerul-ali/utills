a114f763a1558f74150d69b06ee87e92f6f834eb
kabeerul-ali <kabeerulali@gmail.com>
2025-01-21 10:19:23 +0530

    i am in hooks befor

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./component/navbar";
+import Textform from "./component/textform";
+function App() {
+  return (
+    <>
+    <Navbar title="Utills"/>
+    <div className="container my=3">
+    <Textform title="Enter  The Text To Analyze"/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/textform.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function textform(props) {
+  return (
+    <>    
+    <h1>{props.title}</h1>
+<div className="textarea">
+  <label for="mybox" class="form-label"></label>
+  <textarea class="form-control" id="mybox" rows="8"></textarea>
+  <button className="btn btn-primary">convert to text</button>
+</div>
+    </>
+  )
+}
